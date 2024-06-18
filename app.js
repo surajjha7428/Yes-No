06f5614d6ae856b2b728ce67178947d55369993d
surajjha7428 <143312238+surajjha7428@users.noreply.github.com>
2024-06-19 01:26:31 +0530

    Done

---
+++ app.js
@@ -0,0 +1,16 @@
+const buttonParent = document.getElementById('buttonParent')
+
+
+function noHandler() {
+    if (buttonParent.style.cssText.includes("column-reverse")) {
+        buttonParent.style.cssText = "flex-direction: column"
+    }
+    else {
+        buttonParent.style.cssText = "flex-direction: column-reverse";
+    }
+
+}
+
+function messageHandler() {
+    window.open("https://api.whatsapp.com/send?phone=917428032210&text=Hi,%20I%20like%20you")
+}
